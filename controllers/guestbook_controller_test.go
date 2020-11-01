c730cf9c9c6ba9d1a47fb63775f7ab0b57266145
Baiju Muthukadan <baiju.m.mail@gmail.com>
2020-11-01 10:34:04 +0530

    filing test
    Signed-off-by: Baiju Muthukadan <baiju.m.mail@gmail.com>

---
+++ controllers/guestbook_controller_test.go
@@ -0,0 +1,106 @@
+package controllers
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	apixv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("Guestbook Controller:", func() {
+	const (
+		timeout       = time.Second * 200
+		interval      = time.Millisecond * 250
+		testNamespace = "default"
+	)
+
+	Context("When creating Guestbook", func() {
+
+		It("should be possible to create custom resource", func() {
+			ctx := context.Background()
+
+			backingServiceCRD := &apixv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "backingservices.app.example.org",
+					Namespace: testNamespace,
+				},
+				Spec: apixv1.CustomResourceDefinitionSpec{
+					Group: "app.example.org",
+					Versions: []apixv1.CustomResourceDefinitionVersion{{
+						Name:    "v1alpha1",
+						Served:  true,
+						Storage: true,
+						Schema: &apixv1.CustomResourceValidation{
+							OpenAPIV3Schema: &apixv1.JSONSchemaProps{
+								Type: "object",
+								Properties: map[string]apixv1.JSONSchemaProps{
+									"status": {
+										Type: "object",
+										Properties: map[string]apixv1.JSONSchemaProps{
+											"binding": {
+												Type: "object",
+												Properties: map[string]apixv1.JSONSchemaProps{
+													"name": {
+														Type: "string",
+													},
+												},
+												Required: []string{"name"},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					},
+					Names: apixv1.CustomResourceDefinitionNames{
+						Plural: "backingservices",
+						Kind:   "BackingService",
+					},
+					Scope: apixv1.ClusterScoped,
+				}}
+			Expect(k8sClient.Create(ctx, backingServiceCRD)).Should(Succeed())
+
+			backingServiceCRDLookupKey := types.NamespacedName{Name: "backingservices.app.example.org", Namespace: testNamespace}
+			createdBackingServiceCRD := &apixv1.CustomResourceDefinition{}
+
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, backingServiceCRDLookupKey, createdBackingServiceCRD)
+				if err != nil {
+					return false
+				}
+				for _, condition := range createdBackingServiceCRD.Status.Conditions {
+					if condition.Type == apixv1.Established &&
+						condition.Status == apixv1.ConditionTrue {
+						return true
+					}
+				}
+				return false
+			}, timeout, interval).Should(BeTrue())
+
+			backingServiceCR := &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"kind":       "BackingService",
+					"apiVersion": "app.example.org/v1alpha1",
+					"metadata": map[string]interface{}{
+						"name": "back1",
+					},
+					"status": map[string]interface{}{
+						"binding": map[string]interface{}{
+							"name": "secret1",
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, backingServiceCR)).Should(Succeed())
+
+		})
+	})
+
+})
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -22,24 +22,25 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	apixv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	webappv1 "github.com/baijum/hellokube2/api/v1"
 	// +kubebuilder:scaffold:imports
 )
 
-// These tests use Ginkgo (BDD-style Go testing framework). Refer to
-// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
-
 var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
+var k8sManager manager.Manager
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -65,10 +66,29 @@ var _ = BeforeSuite(func(done Done) {
 	err = webappv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = apixv1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	// +kubebuilder:scaffold:scheme
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	k8sManager, err = ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
 	Expect(err).ToNot(HaveOccurred())
+
+	err = (&GuestbookReconciler{
+		Client: k8sManager.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Guestbook"),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err := k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
+	k8sClient = k8sManager.GetClient()
 	Expect(k8sClient).ToNot(BeNil())
 
 	close(done)
